83f169736f4904df5d5c79f4618a74a55203d70f
Igor Martins <iguinhoigor4@gmail.com>
2021-03-25 16:58:38 -0300

    feat: Adicionar o index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+axios
+    .get('http://swapi.dev/api/people/1/')
+    .then(function (response) {
+        // handle success
+        console.log(response);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+    });
